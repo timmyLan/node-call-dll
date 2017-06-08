4e4c4b60e9fe960de0bce38dc0e7fb440aacdffe
llan <llan@amwares.com>
2017-06-08 17:48:39 +0800

    change localtion

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var ffi = require('ffi');
-var libfactorial = ffi.Library('./factorial/test.dll', {
+var libfactorial = ffi.Library('C:\Users\llan\code\node-call-dll\factorial', {
     'factorial': ['uint64', ['int']]
 });
 var output = libfactorial.factorial(5);
