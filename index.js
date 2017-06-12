9587e9b74d79a394f7cd9c7ae6804ca089832830
llan <llan@amwares.com>
2017-06-12 10:02:53 +0800

    delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var ffi = require('ffi');
-var libfactorial = ffi.Library('C:/Users/llan/code/node-call-dll/product', {
-    'factorial': ['uint64', ['int']]
-});
-var output = libfactorial.factorial(5);
-console.log('output',output);
